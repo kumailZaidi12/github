238f007460d6ffa7db043ca2352cdcaa47332828
Kumail <kumail@Kumails-MacBook-Pro.local>
2021-07-05 16:36:19 +0530

    conf

--- index.js
+++ index.js
@@ -3,10 +3,5 @@ console.log(variable)
 console.log("hello im in experimental branch")
 console.log("reset after commit ")
 console.log("hello im in experiment 2 branch")
-<<<<<<< HEAD
-console.log("merge and stash")
 console.log("stash 1")
 console.log("stash 2")
-=======
-console.log("merge and stash")
->>>>>>> master
